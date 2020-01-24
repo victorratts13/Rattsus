9e961daa4091f09d31eb59f8638366d12b56b2b4
Victor Ratts <victor.ratts13@gmail.com>
2020-01-24 16:41:59 -0300

    correção de erros
    erro de parada

--- Binance/test.js
+++ Binance/test.js
@@ -46,28 +46,31 @@ setInterval(() => {
                     console.log('erro ao requisitar balanços -> '+JSON.stringify(error))
                 }else{
                     binance.exchangeInfo(function(error, data) {
-                        let minimums = {};
-                        for ( let obj of data.symbols ) {
-                            let filters = {status: obj.status};
-                            for ( let filter of obj.filters ) {
-                                if ( filter.filterType == "MIN_NOTIONAL" ) {
-                                    filters.minNotional = filter.minNotional;
-                                } else if ( filter.filterType == "PRICE_FILTER" ) {
-                                    filters.minPrice = filter.minPrice;
-                                    filters.maxPrice = filter.maxPrice;
-                                    filters.tickSize = filter.tickSize;
-                                } else if ( filter.filterType == "LOT_SIZE" ) {
-                                    filters.stepSize = filter.stepSize;
-                                    filters.minQty = filter.minQty;
-                                    filters.maxQty = filter.maxQty;
-                                }
-                            }
-                            //filters.baseAssetPrecision = obj.baseAssetPrecision;
-                            //filters.quoteAssetPrecision = obj.quoteAssetPrecision;
-                            filters.orderTypes = obj.orderTypes;
-                            filters.icebergAllowed = obj.icebergAllowed;
-                            minimums[obj.symbol] = filters;
+                        if(error){
+                            console.log('um erro ocorreu -> '+error)
                         }
+                                let minimums = {};
+                                for ( let obj of data.symbols ) {
+                                let filters = {status: obj.status};
+                                for ( let filter of obj.filters ) {
+                                    if ( filter.filterType == "MIN_NOTIONAL" ) {
+                                        filters.minNotional = filter.minNotional;
+                                    } else if ( filter.filterType == "PRICE_FILTER" ) {
+                                        filters.minPrice = filter.minPrice;
+                                        filters.maxPrice = filter.maxPrice;
+                                        filters.tickSize = filter.tickSize;
+                                    } else if ( filter.filterType == "LOT_SIZE" ) {
+                                        filters.stepSize = filter.stepSize;
+                                        filters.minQty = filter.minQty;
+                                        filters.maxQty = filter.maxQty;
+                                    }
+                                }
+                                //filters.baseAssetPrecision = obj.baseAssetPrecision;
+                                //filters.quoteAssetPrecision = obj.quoteAssetPrecision;
+                                filters.orderTypes = obj.orderTypes;
+                                filters.icebergAllowed = obj.icebergAllowed;
+                                minimums[obj.symbol] = filters;
+                            }
                         //console.log(minimums.BTCUSDT);
                         global.filters = minimums;
 
